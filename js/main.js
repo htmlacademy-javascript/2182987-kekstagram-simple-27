9f5594f12a454f5222253bae37801f8b949d5c9b
Александр Драган <alexandr.dragan.work@gmail.com>
2022-10-03 23:39:23 +0300

    Исправление проверки параметров функции-рандомайзера & Добавление проверки на положительные значения

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ function getRandomNum(min, max) {
     max = swap;
   }
 
-  if(typeof min === 'number' || typeof max === 'number') {
+  if((typeof min === 'number' && typeof max === 'number') && (min >= 0 && max >= 0)) {
     return Math.floor(min + Math.random() * (max + 1 - min));
   }
   return NaN;
