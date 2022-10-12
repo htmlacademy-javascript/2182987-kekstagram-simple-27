547322e0c4cf48dc75da41211152a8c94145cec1
Александр Драган <alexandr.dragan.work@gmail.com>
2022-10-13 01:29:27 +0300

    Разделение скриптов на модули

---
+++ js/params.js
@@ -0,0 +1,16 @@
+// Количество постов/фото
+const POSTS_QUANTITY = 25;
+
+// Интервал кол-ва лайков
+const LIKES_RANGE = {
+  min: 15,
+  max: 200
+};
+
+// Интервал кол-ва комментариев
+const COMMENTS_RANGE = {
+  min: 0,
+  max: 200
+};
+
+export {POSTS_QUANTITY, LIKES_RANGE, COMMENTS_RANGE};
