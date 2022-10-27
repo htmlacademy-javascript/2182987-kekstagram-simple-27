1f308ef7e91a63f7fb0d7cb64d0c4f8ce748fefa
Keks <keks@htmlacademy.ru>
2022-10-28 02:26:58 +0300

    :heavy_check_mark: Сборка #6

--- 6/js/form.js
+++ 6/js/form.js
@@ -1,10 +1,25 @@
-import {isEscape} from './utils.js';
+import {checkStringLengthRange, isEscape, isFileImage} from './utils.js';
+import {COMMENT_LENGTHS} from './params.js';
 
 const body = document.querySelector('body');
 const form = document.querySelector('#upload-select-image');
 const uploadImgInput = document.querySelector('#upload-file');
+const postCommentInput = document.querySelector('.text__description');
 const uploadFormOverlay = document.querySelector('.img-upload__overlay');
 const closeUploadBtn = document.querySelector('#upload-cancel');
+const errorsOutput = document.querySelector('.img-upload__errors-output');
+
+// Добавление ошибки
+function setErrors() {
+  errorsOutput.classList.remove('hidden');
+  errorsOutput.textContent = getFormErrors();
+}
+
+// Удаление ошибки
+function removeErrors() {
+  errorsOutput.classList.add('hidden');
+  errorsOutput.textContent = '';
+}
 
 // Функция-прослушка события нажания Esc
 const onEscKeydown = (evt) => {
@@ -20,6 +35,7 @@ function showImgEditor () {
   uploadFormOverlay.classList.remove('hidden');
   closeUploadBtn.addEventListener('click', closeImgEditor);
   document.addEventListener('keydown', onEscKeydown);
+  removeErrors();
 }
 
 // Закрытие редактора фото
@@ -30,6 +46,30 @@ function closeImgEditor () {
   document.removeEventListener('keydown', onEscKeydown);
   form.reset();
 }
+
+function getFormErrors() {
+  if(!isFileImage(uploadImgInput.value)) {
+    return 'Недопустимый формат файла. Вы можете использовать изображения в одном из перечисленных форматов: jpg, jpeg, png, webp';
+  }
+
+  if(!checkStringLengthRange(postCommentInput.value, COMMENT_LENGTHS.min, COMMENT_LENGTHS.max)) {
+    postCommentInput.classList.add('text__description_error');
+    return 'Длина комментария не может быть меньше 20 символов и больше 140 символов';
+  }
+
+  postCommentInput.classList.remove('text__description_error');
+  return false;
+}
+
+// Отправка формы
+form.addEventListener('submit', (evt) => {
+  if(getFormErrors()) {
+    evt.preventDefault();
+    setErrors();
+  } else {
+    removeErrors();
+  }
+});
 
 // Обработчик события добавления/изменения фото
 uploadImgInput.addEventListener('change', () => {
