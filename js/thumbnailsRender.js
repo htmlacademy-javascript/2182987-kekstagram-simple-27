d063194c034b6f9466a47b931a339e06d49d255a
Александр Драган <alexandr.dragan.work@gmail.com>
2022-11-13 03:17:12 +0200

    Использование постов с удаленного сервиса

--- js/thumbnailsRender.js
+++ js/thumbnailsRender.js
@@ -1,13 +1,18 @@
-import {posts} from './data.js';
-
 const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
 const pictures = document.querySelector('.pictures');
 
-posts.forEach((post) => {
-  const thumbnail = pictureTemplate.cloneNode(true);
+const getPosts = () => {
+  fetch('https://27.javascript.pages.academy/kekstagram-simple/data')
+    .then((response) => response.json())
+    .then((posts) => {
+      posts.forEach((post) => {
+        const thumbnail = pictureTemplate.cloneNode(true);
+        thumbnail.querySelector('.picture__img').src = post.url;
+        thumbnail.querySelector('.picture__comments').innerText = post.comments;
+        thumbnail.querySelector('.picture__likes').innerText = post.likes;
+        pictures.appendChild(thumbnail);
+      });
+    });
+};
 
-  thumbnail.querySelector('.picture__img').src = post.url;
-  thumbnail.querySelector('.picture__comments').innerText = post.comments;
-  thumbnail.querySelector('.picture__likes').innerText = post.likes;
-  pictures.appendChild(thumbnail);
-});
+getPosts();
