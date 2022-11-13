874176c5922a0ff7dd08ad407fc0c109f5435663
Александр Драган <alexandr.dragan.work@gmail.com>
2022-11-13 04:28:14 +0200

    Ошибка сервера - функционал и стили

--- js/api/api.js
+++ js/api/api.js
@@ -1,8 +1,37 @@
+function showError(message) {
+  const body = document.querySelector('body');
+  const errorBlock = document.createElement('div');
+  errorBlock.style.position = 'absolute';
+  errorBlock.style.top = '16px';
+  errorBlock.style.left = '16px';
+  errorBlock.style.width = '200px';
+  errorBlock.style.maxWidth = '100vw';
+  errorBlock.style.padding = '8px';
+  errorBlock.style.borderRadius = '12px';
+  errorBlock.style.fontSize = '12px';
+  errorBlock.style.textAlign = 'center';
+  errorBlock.style.backgroundColor = '#cb5b5b';
+  errorBlock.style.color = '#ffffff';
+  errorBlock.style.opacity = '0';
+  errorBlock.style.transition = 'opacity 0.4s';
+  errorBlock.innerText = message;
+  setTimeout(() => {
+    errorBlock.style.opacity = '1';
+  }, 200);
+  setTimeout(() => {
+    errorBlock.remove();
+  }, 2000);
+  body.append(errorBlock);
+}
+
 const getPosts = (onSuccess) => {
   fetch('https://27.javascript.pages.academy/kekstagram-simple/data')
     .then((response) => response.json())
     .then((posts) => {
       onSuccess(posts);
+    })
+    .catch((err) => {
+      showError(err.message);
     });
 };
 
